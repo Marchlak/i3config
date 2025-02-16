f0b390c24af7c38956da724c9f3d09716baf222e
marchlak <bingor1@wp.pl>
2025-02-16 15:59:47 +0100

    aha32

---
+++ config.d/scripts/monitors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --primary
+xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1920x0
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 384
